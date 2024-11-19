46b7348c529362912728178df2095c8c513760ce
lesalonge <leslieaalonge@gmail.com>
2024-11-18 21:19:34 -0500

    created TeamMemberContextDAO.cs

---
+++ IT3045-Final-Group4/Data/TeamMemberContextDAO.cs
@@ -0,0 +1,49 @@
+using IT3045_Final_Group4.Interfaces;
+using IT3045_Final_Group4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT3045_Final_Group4.Data
+{
+    public class TeamMemberContextDAO : ITeamMemberContextDAO
+    {
+
+        private readonly TeamMemberContext _context;
+        public TeamMemberContextDAO(TeamMemberContext context)
+        {
+            _context = context;
+        }
+
+        public List<TeamMember> GetAllMembers()
+        {
+            return _context.TeamMembers.ToList();
+        }
+
+        public TeamMember GetMemberById(int id)
+        {
+            return _context.TeamMembers.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+         public void AddMember(TeamMember teamMember)
+        {
+            _context.TeamMembers.Add(teamMember);
+            _context.SaveChanges();
+        }
+
+        public void UpdateMember(TeamMember teamMember)
+        {
+            _context.TeamMembers.Update(teamMember);
+            _context.SaveChanges();
+        }
+
+        public void DeleteMember(int id)
+        {
+            // find team member by id and delete
+            var member = _context.TeamMembers.FirstOrDefault(x => x.Id == id);
+            if (member != null)
+            {
+                _context.TeamMembers.Remove(member);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
