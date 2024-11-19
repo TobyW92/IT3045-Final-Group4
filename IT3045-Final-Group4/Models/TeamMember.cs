84fe4632a5ffa753036c667c50e2d2354c3a58d1
lesalonge <leslieaalonge@gmail.com>
2024-11-18 21:07:44 -0500

    created TeamMember.cs model

---
+++ IT3045-Final-Group4/Models/TeamMember.cs
@@ -0,0 +1,15 @@
+namespace IT3045_Final_Group4.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string CollegeProgram {get; set; }
+
+        public string YearInProgram ( get; set;)
+    }
+}
