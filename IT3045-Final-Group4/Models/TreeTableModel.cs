60fd0c0ae1d20d16cb1c19b683d250c8ab0f12ba
Matt Glazier <glaziemj@mail.uc.edu>
2024-12-05 16:35:47 -0500

    Added in the model for Tree Sale Table

---
+++ IT3045-Final-Group4/Models/TreeTableModel.cs
@@ -0,0 +1,21 @@
+﻿namespace IT3045_Final_Group4.Models
+{
+    public class TreeTableModel
+    {
+        // Primary Key
+        public int Id { get; set; }
+        // Type of Tree
+        public string TreeType { get; set; }
+        // Height of Tree
+        public double Height { get; set; }
+        // Name of Person
+        public string BroughtBy { get; set; }
+        // Email
+        public string Email { get; set; }
+        // Phone Number
+        public string PhoneNumber { get; set; }
+        // Birthdate
+        public string Birthday { get; set; }
+    }
+
+}
