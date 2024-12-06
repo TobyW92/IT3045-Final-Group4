ba11eb9839530a903e6c7f2c25d0ddb83f330a19
TobyW92 <tobywright@live.com>
2024-12-05 20:34:34 -0500

    Updated program.cs

--- IT3045-Final-Group4/Program.cs
+++ IT3045-Final-Group4/Program.cs
@@ -14,13 +14,12 @@ builder.Services.AddSwaggerDocument();
 // Add Context Scope
 builder.Services.AddScoped<ITreeTableContextDAO, TreeTableContextDAO>();
 builder.Services.AddScoped<ISampleContextDAO, SampleContextDAO>();
-        // TEST
 builder.Services.AddScoped<ITeamMemberContextDAO, TeamMemberContextDAO>();
 
 // Add DB Context
 builder.Services.AddDbContext<SampleContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
-        // TEST
-builder.Services.AddDbContext<TeamMemberContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"))); 
+builder.Services.AddDbContext<TeamMemberContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+builder.Services.AddDbContext<TreeTableContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 
 var app = builder.Build();
 
@@ -34,9 +33,11 @@ using (var scope = app.Services.CreateScope())
     
     var services = scope.ServiceProvider;
     var db = services.GetRequiredService<SampleContext>();
-    db.Database.Migrate(); 
+    //db.Database.Migrate(); 
     var teamDb = services.GetRequiredService<TeamMemberContext>();
-teamDb.Database.Migrate(); // TEST 
+    //teamDb.Database.Migrate();
+    var treeDb = services.GetRequiredService<TreeTableContext>();
+    //treeDb.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
